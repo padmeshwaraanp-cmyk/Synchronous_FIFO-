70122070b21eead6218a073bf98cd0d0021c5676
padmeshwaraanp-cmyk <padmeshwaraanp@gmail.com>
2026-01-09 11:33:39 +0530

    Update fifo.v

--- fifo.v
+++ fifo.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 14.10.2025 19:51:35
-// Design Name: 
-// Module Name: fifo
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module fifo #(parameter m=4, n=16) (
